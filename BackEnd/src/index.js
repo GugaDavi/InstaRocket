4779be214cba1f62027a327553bc31d068af7f09
GugaDavi <gguedes.tech@gmail.com>
2019-06-15 23:41:00 -0300

    add Cors and websocket

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
 
 const App = express()
+
+const server = require('http').Server(App)
+const io = require('socket.io')(server)
 
 mongoose.connect("mongodb+srv://gugaadmin:Lovegod777*@cluster0-iosms.mongodb.net/test?retryWrites=true&w=majority", {
   useNewUrlParser: true
 })
 
+App.use((req, res, next) => {
+  req.io = io
+
+  next()
+})
+
+App.use(cors())
+
+App.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+
 App.use(require('./routes'))
 
-App.listen(3005)
+server.listen(3005)
