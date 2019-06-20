54f28687d4a266949ef26478a487629426291073
GugaDavi <gguedes.tech@gmail.com>
2019-06-19 22:59:17 -0300

    Feed

--- instaRocket/src/pages/Feed/index.js
+++ instaRocket/src/pages/Feed/index.js
@@ -1,11 +1,124 @@
 import React, { Component } from 'react';
+import api from '../../services/api'
 
-import { View } from 'react-native';
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  StyleSheet
+} from 'react-native';
 
-// import { Container } from './styles';
+import camera from '../../assets/camera.png'
+import like from '../../assets/like.png'
+import more from '../../assets/more.png'
+import send from '../../assets/send.png'
+import comment from '../../assets/comment.png'
 
+import { FlatList } from 'react-native-gesture-handler';
 export default class Feed extends Component {
-  render() {
-    return <View />;
+  static navigationOptions = ({ navigation }) => ({
+    henderRight: (
+      <TouchableOpacity style={{ marginRight: 20 }} onPress={() => navigation.navigate('News')}>
+        <Image source={camera}/>
+      </TouchableOpacity>
+    )
+  })
+  state =  {
+    feed: []
   }
+  async componentDidMount() {
+    // this.registerToSocket();
+
+    const response = await api.get('posts')
+
+    this.setState({ feed: response.data })
+  }
+
+  render() {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={this.state.feed}
+        keyExtractor={post => post._id}
+        renderItem={({ item }) => (
+          <View style={styles.feedItem}>
+            <View style={styles.feedItemHeder}>
+              <View style={styles.userInfo}>
+                <Text style={styles.name}>{item.author}</Text>
+                <Text style={styles.place}>{item.place}</Text>
+              </View>
+              <Image source={more}/>
+            </View>
+            <Image style={styles.feedImage} source={{ uri: `http://10.0.2.2:3333/files/${item.image}` }}/>
+            <View style={styles.feedItemFooter}>
+              <View style={styles.actions}>
+                <TouchableOpacity style={styles.action}>
+                  <Image source={like}/>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.action}>
+                  <Image source={comment}/>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.action}>
+                  <Image source={send}/>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.textFooter}>
+                <Text style={styles.likes}>{item.likes} Curtidas</Text>
+                <Text style={styles.description}>{item.description}</Text>
+                <Text style={styles.hashtags}>{item.hashtags}</Text>
+              </View>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+  )
 }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  feedItem: {
+    marginTop: 20,
+  },
+  feedItemHeder: {
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  name: {
+    fontSize: 14,
+    color: '#000',
+  },
+  place: {
+    fontSize: 12,
+    color: '#666',
+    marginTop: 2,
+  },
+  feedImage: {
+    width: '100%',
+    height: 400,
+    marginVertical: 15,
+  },
+  feedItemFooter: {
+    paddingHorizontal: 15,
+  },
+  actions: {
+    flexDirection: 'row',
+  },
+  action: {
+    marginRight: 8,
+  },
+  likes: {
+    marginTop: 15,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  hashtags: {
+    color: '#7159c1'
+  }
+})
